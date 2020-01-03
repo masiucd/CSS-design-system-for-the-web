82146272e81b1bf0e3e64be7393dadaba4f27c08
Masiu <ciszekmarcell@gmail.com>
2020-01-03 17:25:32 +0100

    add

--- intro/mindSweaper/index.js
+++ intro/mindSweaper/index.js
@@ -4,15 +4,35 @@
  * @returns {number[][]}
  */
 function minesweeper(matrix) {
-  const result = [].concat(...matrix).map(x => (x === true ? 1 : 2));
+  // const result = [].concat(...matrix).map(x => (x === true ? 1 : 2));
 
-  console.log(result);
+  // let mind;
 
-  let x;
+  // console.log(result);
 
-  for (let i = 0; i < result.length / matrix[0].length; i++) {
-    console.log(i);
+  // console.log(result);
+  // for (let i = 0; i < result.length / matrix[0].length; i++) {}
+
+  // const foo = new Array(45).fill([]); // create an empty array with length 45
+
+  const arr = [];
+  for (let i = 0; i < matrix.length; i++) {
+    arr.push([]);
+    for (let j = 0; j < matrix[i].length; j++) {
+      arr[i][j] = 0;
+
+      if (matrix[i][j - 1] === true) {
+        arr[i][j] += 1;
+      }
+      if (matrix[i][j + 1] !== undefined) {
+        if (matrix[i][j + 1] === true) {
+          arr[i][j] += 1;
+        }
+      }
+    }
   }
+
+  console.log(arr);
 }
 
 const a = [
@@ -20,4 +40,13 @@ const a = [
   [false, true, false],
   [false, false, false],
 ];
-console.log(minesweeper(a));
+
+const b = [
+  [true, false],
+  [true, false],
+  [false, true],
+  [false, false],
+  [false, false],
+];
+// console.log(minesweeper(a));
+console.log(minesweeper(b));
