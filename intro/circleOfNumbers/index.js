95fe923573f1195f85b639237b0b2791e30ebd12
Masiu <ciszekmarcell@gmail.com>
2020-01-07 10:04:55 +0100

    add

---
+++ intro/circleOfNumbers/index.js
@@ -0,0 +1,13 @@
+/**
+ *
+ * @param {number} n
+ * @param {number} firstNumber
+ * @returns {number}
+ */
+function circleOfNumbers(n, firstNumber) {
+  //
+}
+
+console.log(circleOfNumbers(10, 2)); // 7
+console.log(circleOfNumbers(6, 3)); // 0
+// console.log(circleOfNumbers(10, 7)); // 2
--- intro_gates/circleofNums/index.js
+++ intro_gates/circleofNums/index.js
@@ -1,18 +1,19 @@
 /* eslint-disable prefer-const */
 function circleOfNumbers(n, firstNumber) {
-    var nums = [];
-    var halfWay = n / 2;
-    for (var i = 0; i < n; i += 1) {
-        nums.push(i);
-    }
-    // if it is on the first half of the circle
-    if (firstNumber < halfWay) {
-        return nums[firstNumber + halfWay];
-    }
-    if (firstNumber > halfWay) {
-        return nums[firstNumber - halfWay];
-    }
+  let nums = [];
+  let halfWay = n / 2;
+  for (let i = 0; i < n; i += 1) {
+    nums.push(i);
+  }
+  // if it is on the first half of the circle
+  if (firstNumber < halfWay) {
+    return nums[firstNumber + halfWay];
+  }
+  if (firstNumber > halfWay) {
+    return nums[firstNumber - halfWay];
+  }
 }
-console.log(circleOfNumbers(10, 2)); // 7
-console.log(circleOfNumbers(10, 7)); // 2
+// console.log(circleOfNumbers(10, 2)); // 7
+// console.log(circleOfNumbers(10, 7)); // 2
+console.log(circleOfNumbers(6, 3)); // 0
 // console.log(circleOfNumbers(4, 1)); // 3
