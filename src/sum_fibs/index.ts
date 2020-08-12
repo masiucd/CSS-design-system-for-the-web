08b121f870fe56972f5d9efa7fac802076c1907e
Masiu <ciszekmarcell@gmail.com>
2020-08-12 09:53:18 +0200

    sum fibs

---
+++ src/sum_fibs/index.ts
@@ -0,0 +1,17 @@
+function sumFibs(num: number): number {
+  let prev = 0;
+  let current = 1;
+  let res = 0;
+  while (current <= num) {
+    if (current % 2 !== 0) {
+      res += current;
+    }
+    current += prev;
+    prev = current - prev;
+  }
+  return res;
+}
+
+// console.log(sumFibs(4));
+// console.log(sumFibs(7));
+// console.log(sumFibs(1000));
