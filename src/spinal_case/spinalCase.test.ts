3a798ee80c336cad3e716766d7d242295b6929a6
Masiu <ciszekmarcell@gmail.com>
2020-08-14 17:40:34 +0200

    tests

--- src/spinal_case/spinalCase.test.ts
+++ src/spinal_case/spinalCase.test.ts
@@ -4,7 +4,9 @@ describe("spinalCase", () => {
   test("fn", () => {
     expect(typeof spinalCase).toBe("function");
     expect(spinalCase).toBeDefined();
+    let x = spinalCase("This Is Spinal Tap");
     expect(spinalCase("This Is Spinal Tap")).toBe("this-is-spinal-tap");
     expect(spinalCase("The_Andy_Griffith_Show")).toBe("the-andy-griffith-show");
+    expect(typeof x).toBe("string");
   });
 });
